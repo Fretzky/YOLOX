f657dd94ba6e64e2ae9a95adb38d6b9058c1bb7b
Frederic Goretzky <fredericgoretzky@gmail.com>
2026-02-17 12:09:28 +0100

    update of exp file for transferlearning to use half precision and smaller batch size

--- exps/example/woodscape/yolox_m_woodscape_transfer.py
+++ exps/example/woodscape/yolox_m_woodscape_transfer.py
@@ -5,8 +5,12 @@ import os
 from yolox.exp import Exp as MyExp
 """
 
-Experiment Reference Pipeline for YOLOX_m on WoodScape Dataset
+Experiment Transferlearning Pipeline for YOLOX_m on WoodScape Dataset only on Person class.
 results in results/p1_reference_yolox_m
+
+The dataset is prefiltered to only contain Person class annotations.
+The pretrained model is the official yolox_m model trained on COCO with 80 classes.
+
         
 """
 
@@ -19,13 +23,18 @@ class Exp(MyExp):
         
         self.num_classes = 80 # default from pretrained model.
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
-         
-        # Define yourself dataset path
-        self.data_dir = os.environ.get("WOODSCAPE_COCO_DIR", "/app/data/woodscape_coco")
+        self.freeze_backbone = True # freeze backbone for transfer learning, only train head.
+        
+        # for training on HCI cluster with 48 GiB GPU(A40?), 
+        # half precision training is recommended to save GPU memory and speed up training.
+        self.fp16 = True
+        self.batch_size = 16
+        
+        self.data_dir = "../dataset/woodscape_coco/"
         self.train_ann = "train.json"
         self.test_ann = "test.json"
         self.val_ann = "val.json"
-        self.image_size = (1963, 1216) # original image size, but we will resize to test_size for evaluation. 
+        # self.image_size = (1963, 1216) # original image size, but we will resize to test_size for evaluation. 
         # --------------  training config --------------------- #
         # epoch number used for warmup
         self.warmup_epochs = 5
@@ -35,7 +44,7 @@ class Exp(MyExp):
         self.warmup_lr = 0
         self.min_lr_ratio = 0.05
         # learning rate for one image. During training, lr will multiply batchsize.
-        self.basic_lr_per_img = 0.01 / 64.0
+        self.basic_lr_per_img = 0.001 / 64.0 ## reduced lr for transfer learning, original is 0.01 / 64.0
         # name of LRScheduler
         self.scheduler = "yoloxwarmcos"
         # last #epoch to close augmention like mosaic
@@ -52,7 +61,7 @@ class Exp(MyExp):
         self.print_interval = 10
         # eval period in epoch, for example,
         # if set to 1, model will be evaluate after every epoch.
-        self.eval_interval = 10
+        self.eval_interval = 10 # todo maybe test every 5
         # save history checkpoint or not.
         # If set to False, yolox will only save latest and best ckpt.
         self.save_history_ckpt = True
@@ -75,20 +84,18 @@ class Exp(MyExp):
             data_dir=self.data_dir,
             json_file=self.train_ann,
             name="images/train",
-            img_size=self.image_size,
-            preproc=TrainTransform(),
-            cache=cache,
-            cache_type=cache_type,
+            img_size=self.test_size,
+            preproc=TrainTransform()
+            
         )
-
         return dataset
 
     def get_eval_dataset(self, **kwargs):
         from yolox.data import COCODataset, ValTransform
         dataset = COCODataset(
             data_dir=self.data_dir,
-            json_file=self.val_ann,
-            name="images/val",
+            json_file=self.test_ann,
+            name="images/test",
             img_size=self.test_size,
             preproc=ValTransform(legacy=kwargs.get("legacy", False)),
         )
